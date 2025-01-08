98455884d85314f3e8576324b2176162004ee462
restonic4 <marcosruizlerma@gmail.com>
2025-01-08 23:56:44 +0100

    Utils

---
+++ src/main/java/com/chaotic_loom/under_control/util/ThreadUtils.java
@@ -0,0 +1,18 @@
+package com.chaotic_loom.under_control.util;
+
+import java.util.function.Consumer;
+
+public class ThreadUtils {
+    public static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (Exception ignored) {}
+    }
+
+    public static void runCountDown(int desiredSeconds, Consumer<Integer> consumer) {
+        for (int i = desiredSeconds; i > 0; i--) {
+            consumer.accept(i);
+            sleep(1000);
+        }
+    }
+}
